b492e9688eb48435e8c34d257567b38d5b658eb1
LucasEsti <lie.lucas.razafindrambo@esti.mg>
2019-07-19 14:47:09 +0200

    first commit

---
+++ src/testle/Principal.java
@@ -0,0 +1,14 @@
+package testle;
+
+public class Principal {
+
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ozy ah oe an an an");
+	}
+
+}
